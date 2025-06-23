8008339e7a8d671547aa761e9ef9523aec42f87a
Ritwick Roy <ritwickroy199@gmail.com>
2025-06-24 00:41:48 +0530

    1St commite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy,  Component } from '@angular/core';
 import { ToastsComponent } from 'src/app';
 import { RouterOutlet } from '@angular/router';
 
